ae2c43ddaa1a664bde49e592441ee25110aade44
Runacy <orcinusorca1758dv6932@gmail.com>
2022-11-28 02:47:50 +0900

    add alert

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,17 @@ import '../styles/reset.css';
 import '../styles/main.css';
 
 // This default export is required in a new `pages/_app.js` file.
+const f = () => {
+  if (typeof window === 'object') {
+    window.alert('クリックされたよ！(client)');
+  } else {
+    console.log('クリックされたよ！(server)');
+  }
+};
 export default function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <button onClick={f}>クリック</button>
       {console.log('デプロイしたよーーー')}
       <Component {...pageProps} />
     </div>
